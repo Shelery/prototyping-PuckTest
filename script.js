f014c2ef607577393337e7ca3a56c94ebc3f8f7c
Shelery <92330177+Shelery@users.noreply.github.com>
2023-02-10 19:36:29 +0100

    Add video file

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log("hii");
+const btn1 = document.querySelector("#btn1");
+console.log(btn1);
+const btn2 = document.querySelector("#btn2");
+console.log(btn2);
+btn1.addEventListener("click", () => {
+  Puck.write("LED1.set();\n");
+});
+btn2.addEventListener("click", () => {
+  Puck.write("LED1.reset();\n");
+});
